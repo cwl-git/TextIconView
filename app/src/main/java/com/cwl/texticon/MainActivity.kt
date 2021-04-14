c82dcd40fd4380387e2dac7f3c7d8e6f441dc082
cwl <cqxlink@163.com>
2021-04-14 14:56:11 +0800

    <feat>添加动态设置属性样例代码

--- app/src/main/java/com/cwl/texticon/MainActivity.kt
+++ app/src/main/java/com/cwl/texticon/MainActivity.kt
@@ -1,11 +1,27 @@
 package com.cwl.texticon
 
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.Color
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        //动态修改TextIconView的各属性
+        text_icon_view_bg.apply {
+            setOnLongClickListener {
+                val textBgDrawable = resources.getDrawable(R.drawable.shape_conner_5_purple, null)
+                val iconDrawable = resources.getDrawable(R.drawable.ic_play,null)
+                mLeftText = "新的文本"
+                mLeftTextBg = textBgDrawable
+                mLeftTextColor = Color.WHITE
+                mLeftTextSize = resources.getDimension(R.dimen.common_text_size)
+                mIconMarginLeft = resources.getDimension(R.dimen.common_text_size)
+                mIcon = iconDrawable
+                return@setOnLongClickListener true
+            }
+        }
     }
 }
